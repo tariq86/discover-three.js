4ba541d6f0f9158f08e5f40212e91a0070b3a8e0
Tariq <646561+tariq86@users.noreply.github.com>
2020-10-29 10:13:46 -0500

    Enable SSL

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const express = require('express');
+const fs = require('fs');
+
+let key;
+let cert;
+try {
+    key = fs.readFileSync(__dirname + '/keys/key.pem');
+    cert = fs.readFileSync(__dirname + '/keys/cert.pem');
+} catch (err) {
+    console.warn('Could not read SSL key and/or cert; creating server without HTTPS');
+}
+
+const PORT = process.env.PORT || 8443;
 
 const app = express();
+app.disable('x-powered-by');
 
 app.use((request, response, next) => {
-    console.log(`*** Incoming request: ${request.method} ${request.url}`);
+    console.log(`==> Incoming request: ${request.method} ${request.url}`);
     next();
 });
 
@@ -13,6 +26,19 @@ app.get('/', (req, res) => {
     return res.sendFile(`${__dirname}/index.html`);
 });
 
-app.listen(8080, () => {
-    console.debug('Server is live @ http://localhost:8080');
-})
+const serverOnInit = () => {
+    let proto = 'http';
+    if (key && cert) {
+        proto += 's';
+    }
+    console.debug(`🌎 Server is live @ ${proto}://localhost:${PORT}`);
+}
+
+if (key && cert) {
+    const https = require('https');
+    console.debug('*** SSL key and cert found, enabling SSL ***');
+    const server = https.createServer({ cert, key }, app);
+    server.listen(PORT, serverOnInit);
+} else {
+    app.listen(PORT, serverOnInit);
+}
