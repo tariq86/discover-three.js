64677712d6baa90d3a9014e6483d73db5e7fbed9
Tariq <646561+tariq86@users.noreply.github.com>
2020-10-29 10:37:27 -0500

    Add "Pause scene" button

--- public/main.js
+++ public/main.js
@@ -1,19 +1,34 @@
 import { World } from './src/World/World.js';
 
-function main() {
+let world;
+function createWorld() {
     // Get a reference to the container element
     const container = document.querySelector('#scene-container');
 
-    // 1. Create an instance of the World app
-    const world = new World(container);
+    // 1. Create an instance of the World app (if it doesn't exist yet)
+    if (!world) {
+        world = new World(container);
+    }
 
     // 2. Render the scene
     // world.render();
     world.start();
 }
 // Start the scene + toggle the button visibility when "Start" button is clicked
-document.querySelector('#start-scene').addEventListener('click', function (evt) {
+const startBtn = document.querySelector('#start-scene');
+const stopBtn = document.querySelector('#stop-scene');
+startBtn.addEventListener('click', function (evt) {
     console.log('Starting the scene!');
-    main();
-    evt.target.style.display = 'none';
+    createWorld();
+    startBtn.style.display = 'none';
+    stopBtn.style.display = 'block';
+});
+
+stopBtn.addEventListener('click', function (evt) {
+    console.log('Pausing the scene!');
+    startBtn.style.display = 'block';
+    stopBtn.style.display = 'none';
+    if (world) {
+        world.stop();
+    }
 });
